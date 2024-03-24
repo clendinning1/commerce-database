c117a9484c8a0af7d2ddd9c2cc8f419956c874b6
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-03-24 11:14:06 -0500

    commenting first model to better understand code

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD, {
   // needs db name, user, and pass in it. they are brought in through variables from the .env file ^
 
   host: 'localhost',
--- models/Category.js
+++ models/Category.js
@@ -1,8 +1,15 @@
+// importing Model and DataTypes from our ORM sequelize
 const { Model, DataTypes } = require('sequelize');
-
+// importing sequelize from connection.js
 const sequelize = require('../config/connection.js');
 
+// creating a new Sequelize model for Category
+// extending the Model means it's inheriting the Model class (which we import frm Sequelize)
 class Category extends Model {}
+
+// don't have anything in the class when we first define it,
+// but then we use the .init method after the class.
+// .init method is from Model{}. it's inheritance. 
 
 Category.init(
   {
