13cdff4694a917c9a8e91832c026ad09b180aaff
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-03-24 13:59:50 -0500

    built the product/category relationship

--- models/index.js
+++ models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  // foreignKey is the name of the foreign key in the Product.js file!
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+// Product.belongsToMany( , {
+// so, using "Tag.js" or "ProductTag.js" ?
+// });
 
 // Tags belongToMany Products (through ProductTag)
+// so, using "Tag.js" or "ProductTag.js" ?
+
 
 module.exports = {
   Product,
