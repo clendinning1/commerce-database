92e16c30f089da5a68e4d1b6ed367d2c8e522eb8
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-03-24 11:20:46 -0500

    added comments to category.init to understand what the code's doing. realized what i built in sql yesterday should have all been done in sequelize, but that's ok because it gives me a good reference

--- models/Category.js
+++ models/Category.js
@@ -5,7 +5,7 @@ const sequelize = require('../config/connection.js');
 
 // creating a new Sequelize model for Category
 // extending the Model means it's inheriting the Model class (which we import frm Sequelize)
-class Category extends Model {}
+class Category extends Model { }
 
 // don't have anything in the class when we first define it,
 // but then we use the .init method after the class.
@@ -13,15 +13,25 @@ class Category extends Model {}
 
 Category.init(
   {
+    // first parameter
     // define columns
   },
   {
+    // second parameter
+    // link to db conection
     sequelize,
+    // setting to false to remove `created_at` and `updated_at` fields
     timestamps: false,
+    // "If freezeTableName is true, sequelize will not try to alter the DAO name to get the table name. Otherwise, the dao name will be pluralized."
+    // ?? i think it automatically makes every column and/or table name plural unless freezeTableName is true, but unsure
     freezeTableName: true,
+    // converts all camelcase columns to underscored
+    // ie, isPaperback -> is_paperback
     underscored: true,
+    // name of the table
     modelName: 'category',
   }
 );
 
+// exporting the table
 module.exports = Category;
