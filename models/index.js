9c45b11b9d97823082d8ba96814f77955c411926
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-03-24 20:58:24 -0500

    misnamed keys in ProductTag.js & models/index.js

--- models/index.js
+++ models/index.js
@@ -4,28 +4,28 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-Product.belongsTo(Category, {
-  // foreignKey is the name of the foreign key in the Product.js file!
-  foreignKey: 'category_id',
-});
-
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
 });
 
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  // foreignKey is the name of the foreign key in the Product.js file!
+  foreignKey: 'category_id',
+});
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'product_tag',
+  foreignKey: 'tag_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: 'product_tag',
+  foreignKey: 'product_id',
 });
 
 
